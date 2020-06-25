560928b4f0c74eb06511b388edd4de5fec77c680
Caleb R <cjr4912@gmail.com>
2020-06-25 16:27:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 
     "Fizz"
-    elsif int % 5
-      "Buzz"
-      elsif int % 5 && int % 3
-        "FizzBuzz"
-      end
-    end
+  elsif int % 5
+    "Buzz"
+  elsif int % 5 && int % 3
+    "FizzBuzz"
   end
-end 
-
+end
